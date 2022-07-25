eabd9d9f39092d57429a5d6b52c7b6b4d8238cfc
DUOLabs333 <dvdugo333@gmail.com>
2022-07-24 22:43:59 -0400

    In container.py: Move self.netns to initialization of Container
    Move helper functions to _utils.misc (may move underscored functions there soon)

--- container.py
+++ container.py
@@ -26,52 +26,7 @@ utils.GLOBALS=globals()
 
 SHELL_CWD=os.environ.get("PWD")
 
-#Helper functions  
-def convert_colon_string_to_directory(string):
-    string=utils.split_string_by_char(string,char=":")
-    if string[0]=="root":
-        string=string[1] #The directory is just the absolute path in the host
-    elif len(string)==1:
-        string=string[0] # No container was specified, so assume "root"
-    else:
-        string=f"{utils.ROOT}/{string[0]}/diff{string[1]}" # Container was specified, so use it
-    string=os.path.expanduser(string)
-    return string
-    
-def is_port_in_use(port) :
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(('127.0.0.1', port)) == 0
-def remove_empty_folders_in_diff():
-    walk = list(os.walk("diff"))
-    for path, _, _ in walk[::-1]:
-        if not path.startswith("diff/.unionfs"):
-            if len(os.listdir(path)) == 0:
-                os.rmdir(path)
-def get_all_items(root):
-    #Implement Depth-First Search through utils.ROOT
-    items=[]
-    stack=[root]
-    visited={}
-    while len(stack)>0:
-        v=stack.pop()
-        if v not in visited:
-            #Visit
-            visited[v]=True
-            if os.path.isfile(os.path.join(v,"container-compose.py")):
-                items.append(os.path.relpath(v,root)) #Don't need full path
-                continue #No need to search deeper
-            if len(os.listdir(v))==1 and os.listdir(v)[0]=="diff":
-                continue #If there's nothing but diff, no need to search deeper
-            
-            for w in os.listdir(v):
-                w=os.path.join(v,w)
-                if w not in visited:
-                    stack.append(w)
-    return items
-
-utils.get_all_items=get_all_items              
-def str2bool(v):
-  return v.lower() in ("yes", "true", "t", "1")
+utils.get_all_items=_utils.misc.get_all_items
   
 class Container:
     def __init__(self,_name,_flags={},_unionopts=None,_workdir='/',_env=None,_uid=None,_gid=None,_shell=None):
@@ -91,7 +46,7 @@ class Container:
         #Whether we mounted dev, proc, etc.
         self.mounted_special=False
         
-        self.namespaces={"user":str2bool(os.getenv("CONTAINER_USER_NAMESPACES","0")),"net":str2bool(os.getenv("CONTAINER_NET_NAMESPACES","0"))}
+        self.namespaces={"user":_utils.misc.str2bool(os.getenv("CONTAINER_USER_NAMESPACES","0")),"net":_utils.misc.str2bool(os.getenv("CONTAINER_NET_NAMESPACES","0"))}
         
         self.workdir=_workdir
         
@@ -109,7 +64,7 @@ class Container:
         
         self.base=False
         self.ports=[]
-        
+        self.netns=f"{self.normalized_name}-netns"
         self.setup=False #Whether _setup was run once
         
                     
@@ -213,7 +168,6 @@ class Container:
                 self.mounted_special=True
                 
                 if self.namespaces['net']: #Start network namespace
-                    self.netns=f"{self.normalized_name}-netns"
                     self.veth_pair={"netns":{"name":f"{self.normalized_name}-veth0"},"host":{"name":f"{self.normalized_name}-veth1"}}
                     #self.veth_pair=types.SimpleNamespace(netns=types.SimpleNamespace(name=f"{self.normalized_name}-veth0"),host=types.SimpleNamespace(name=f"{self.normalized_name}-veth1"))
                     
@@ -261,7 +215,7 @@ class Container:
             return list(map(int,processes))
     
     def Mount(self,IN,OUT):
-        IN=convert_colon_string_to_directory(IN)
+        IN=_utils.misc.convert_colon_string_to_directory(IN)
         if os.path.isdir(IN):
             try:
                 os.makedirs(f"diff{OUT}",exist_ok=True)
@@ -287,7 +241,7 @@ class Container:
         else:
             dest=f"diff{dest}"
         
-        src=convert_colon_string_to_directory(src)
+        src=_utils.misc.convert_colon_string_to_directory(src)
         #Relative directory
         if not src.startswith("/"):
             src=f"{SHELL_CWD}/{src}"
@@ -381,7 +335,7 @@ class Container:
         _from=int(_from)
         _to=int(_to)
         
-        if is_port_in_use(_to): #Port is in use, so leave
+        if _utils.misc.is_port_in_use(_to): #Port is in use, so leave
             return 
         if _to in self.ports:
             return
@@ -473,7 +427,7 @@ class Container:
         utils.execute(self,open("Containerfile.py"))
 
         self.Stop()
-        remove_empty_folders_in_diff()
+        _utils.misc.remove_empty_folders_in_diff()
         for layer in self.temp_layers:
             #Clean layer if it was temporary
             self.__class__(layer).Clean()
--- modules/_utils.py
+++ modules/_utils.py
@@ -3,6 +3,7 @@ module_dict={}
 module_dict["_utils"+os.sep+"misc.py"]="""
 import os, ast, getpass, sys
 
+#Helper functions  
 def load_dependencies(self,root,layer):
     if not os.path.isfile(f\"{root}/{layer}/container-compose.py\"):
         return #Don't error out if container-compose.py doesn't exist
@@ -16,7 +17,7 @@ def load_dependencies(self,root,layer):
                    getattr(self,function)(*arguments) #Run function
 
 def chroot_command(self,command):
-    if self.namespaces.user:
+    if self.namespaces['user']:
         result = [\"unshare\",f\"--map-user={self.uid}\",f\"--map-group={self.gid}\",\"--root=merged\"] #Unshare is available so use it  
     else:
         result = [\"chroot\",f\"--userspec={self.uid}:{self.gid}\", \"merged\"] # Unshare does not exist, so use chroot
@@ -24,15 +25,60 @@ def chroot_command(self,command):
     result+=[f\"{self.shell}\",\"-c\",f\"{self.env}; cd {self.workdir}; {command}\"]
     
     
-    if self.namespaces.net:
+    if self.namespaces['net']:
         result=[\"sudo\",\"ip\",\"netns\",\"exec\",self.netns,\"sudo\",\"-u\",getpass.getuser()]+result
     
-    if sys.platform!=\"cygwin\" and not self.namespaces.user:
+    if sys.platform!=\"cygwin\" and not self.namespaces['user']:
         result=[\"sudo\"]+result
         
     return result
      
+def convert_colon_string_to_directory(string):
+    string=utils.split_string_by_char(string,char=\":\")
+    if string[0]==\"root\":
+        string=string[1] #The directory is just the absolute path in the host
+    elif len(string)==1:
+        string=string[0] # No container was specified, so assume \"root\"
+    else:
+        string=f\"{utils.ROOT}/{string[0]}/diff{string[1]}\" # Container was specified, so use it
+    string=os.path.expanduser(string)
+    return string
+    
+def is_port_in_use(port) :
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('127.0.0.1', port)) == 0
 
+def remove_empty_folders_in_diff():
+    walk = list(os.walk(\"diff\"))
+    for path, _, _ in walk[::-1]:
+        if not path.startswith(\"diff/.unionfs\"):
+            if len(os.listdir(path)) == 0:
+                os.rmdir(path)
+def get_all_items(root):
+    #Implement Depth-First Search through utils.ROOT
+    items=[]
+    stack=[root]
+    visited={}
+    while len(stack)>0:
+        v=stack.pop()
+        if v not in visited:
+            #Visit
+            visited[v]=True
+            if os.path.isfile(os.path.join(v,\"container-compose.py\")):
+                items.append(os.path.relpath(v,root)) #Don't need full path
+                continue #No need to search deeper
+            if len(os.listdir(v))==1 and os.listdir(v)[0]==\"diff\":
+                continue #If there's nothing but diff, no need to search deeper
+            
+            for w in os.listdir(v):
+                w=os.path.join(v,w)
+                if w not in visited:
+                    stack.append(w)
+    return items
+
+           
+def str2bool(v):
+  return v.lower() in (\"yes\", \"true\", \"t\", \"1\")
 """
 module_dict["_utils"+os.sep+"container_docker.py"]="""
 #!/usr/bin/env python
